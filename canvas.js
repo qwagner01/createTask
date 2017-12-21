d81308b310d18300c04f2b52e26a7d70efae4555
Quinn Wagner <qwagner01@gmail.com>
2017-12-21 14:03:43 -0500

    put objects in canvas

--- canvas.js
+++ canvas.js
@@ -5,24 +5,24 @@ var xpos = Math.floor(Math.random() * canvas.width);
 var ypos = Math.floor(Math.random() * canvas.height/4);
 var xvel = 4;
 var yvel = 5;
+var mouseX;
+var mouseY;
+
 
 window.onload = function() {
-  var mouse = reportMousePos(e);
-  var pad = new Paddle("#FFFFFF",mouseX,canvas.height,canvas.width/10,canvas.height/20);
-  var bal = new Ball("FFFFFF",xpos, ypos, canvas.width/100,pad.l,pad.h);
-  console.log(bal.isAlive);
   canvas.addEventListener("click", Canvas);
-  canvas.addEventListener('mousemove', reportMousePos, true);
-  // console.log(mouseX);
+  canvas.addEventListener('mousemove', reportMousePos, false);
 }
 
 
 function Paddle(c,x,y,l,h){
-	this.c = c;this.x = x;this.y = y;this.l = l;this.h = h;
+	this.c = c;this.l = l;this.h = h;
+  this.x = x - this.l/2;
+  this.y = y - this.h/2;
   this.displayVals = function(){console.log(this);}
 	this.drawPaddle = function(){
 	ctx.fillStyle = this.c;
-	ctx.fillRect(this.x + this.l/2,this.y - this.h/2,this.l,this.h);
+	ctx.fillRect(this.x,this.y,this.l,this.h);
   // console.log(this.x+this.l/2);
  }
 }
@@ -35,7 +35,7 @@ function Ball(c,x,y,s,pl,ph){
     		ctx.fillRect(this.x,this.y,this.s,this.s);
 	}
 
-	this.isAlive = function(){return(this.y<canvas.height);}
+	this.isAlive = function(){return this.y<canvas.height;}
 
 	this.play = function(xvel, yvel){
 		this.x += xvel; this.y += yvel;
@@ -53,28 +53,33 @@ function Ball(c,x,y,s,pl,ph){
 }
 
 function Canvas() {
+    var pad = new Paddle("#FFFFFF",mouseX,canvas.height,canvas.width/10,canvas.height/20);
+    var bal = new Ball("FFFFFF",xpos, ypos, canvas.width/100,pad.l,pad.h);
+    console.log(mouseX);
     window.requestAnimationFrame(Canvas);
-    // console.log(mouseX);
-    	ctx.fillStyle = "#000000";
-    	ctx.fillRect(0, 0, canvas.width, canvas.height);
-    	ctx.font = "8px Arial";
-    	ctx.fillStyle = "#ff0000";
-    	ctx.fillText("Score: "+s, 8, 20);
-	    pad.drawPaddle();
-	    bal.drawBall();
-    	bal.play(3,3);
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.font = "8px Arial";
+    ctx.fillStyle = "#ff0000";
+    ctx.fillText("Score: "+s, 8, 20);
+	  pad.drawPaddle();
+	  bal.drawBall();
+    bal.play(3,3);
 }
 
 function reportMousePos(e) {
-    mousePos = getMousePos(canvas, e);
-    return(mousePos);
+    mousePos = getMousePos(canvas, e); //give the heavy lifting to another function
+    mouseX = mousePos.x;
+    mouseY = mousePos.y;
     var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
     // console.log(message);
 }
+
+//define a helper function to do the "heavy lifting" of calculating the mouse coordinates
 function getMousePos(canvas, e) {
     rect = canvas.getBoundingClientRect();
     return {
         x: e.clientX - rect.left,
         y: e.clientY - rect.top
     };
-}
+ };
