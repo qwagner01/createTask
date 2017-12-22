a4d4259217a7aaebec0f528da0ac873c5c90cfc7
Quinn Wagner <qwagner01@gmail.com>
2017-12-22 18:58:42 -0500

    got the ball to move but it gets stuck on walls

--- canvas.js
+++ canvas.js
@@ -3,12 +3,13 @@ var ctx = canvas.getContext('2d');
 var s = 0;
 var xpos = Math.floor(Math.random() * canvas.width);
 var ypos = Math.floor(Math.random() * canvas.height/4);
-var xvel = 4;
-var yvel = 5;
+var xvel = 2;
+var yvel = 2;
 var mouseX;
 var mouseY;
 
 window.onload = function() {
+  // loadScreen();
   canvas.addEventListener("click", Canvas);
   canvas.addEventListener('mousemove', reportMousePos, false);
 }
@@ -21,33 +22,34 @@ function Paddle(c,x,y,l,h){
 	this.drawPaddle = function(){
 	ctx.fillStyle = this.c;
 	ctx.fillRect(this.x,this.y,this.l,this.h);
-  // console.log(this.x+this.l/2);
  }
 }
 
 function Ball(c,x,y,s,pl,ph){
-	this.c = c; this.x = x; this.y = y; this.s = s; this.pl = pl; this.ph = ph;
+	this.c = c; this.s = s; this.pl = pl; this.ph = ph;
+  this.x = x - this.s/2;
+  this.y = y - this.s/2;
   this.displayVals = function() {console.log(this);}
 	this.drawBall = function(){
-    		ctx.fillStyle = this.c;
-    		ctx.fillRect(this.x,this.y,this.s,this.s);
+    ctx.fillStyle = this.c;
+    ctx.fillRect(this.x,this.y,this.s,this.s);
 	}
-
-	this.isAlive = function(){return this.y<canvas.height;}
-
-	this.play = function(xvel, yvel){
-		this.x += xvel; this.y += yvel;
-		xvel += .001; yvel += .001;
-		if (this.x >= canvas.width){xvel *= -1;};
-		if (this.x <= 0){xvel *= -1};
-		if ((this.x >= mouseX - this.pl/2) && (this.x <= mouseX + this.pl/2)){
-			if ((this.y >= canvas.height - this.ph) && (this.y <= canvas.height)){
+	this.isAlive = this.y<canvas.height;
+	this.play = function(){
+		if ((this.x >= canvas.width - this.s) || (this.x <= 0 + this.s)){xvel *= -1;};
+		if ((this.x > mouseX - this.pl/2) && (this.x < mouseX + this.pl/2)){
+			if ((this.y > canvas.height - this.ph) && (this.y < canvas.height)){
 				yvel *= -1;
 				s++;
 		}
-	}
+	 }
 		if (this.y <= 0){yvel *= -1};
 	}
+}
+
+function move(){
+  xpos += xvel; ypos += yvel;
+  xvel += .001; yvel += .001;
 }
 
 function Canvas() {
@@ -61,7 +63,13 @@ function Canvas() {
     ctx.fillText("Score: "+s, canvas.width/20, canvas.height/12);
 	  pad.drawPaddle();
 	  bal.drawBall();
-    bal.play(xvel,yvel);
+    move();
+    bal.play();
+
+
+    if(!bal.isAlive){
+      window.cancelAnimationFrame(Canvas);
+    }
 }
 
 function reportMousePos(e) {
@@ -82,5 +90,13 @@ function getMousePos(canvas, e) {
 return {
   x: (e.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
   y: (e.clientY - rect.top) * scaleY     // been adjusted to be relative to element
+ }
 }
+
+function loadScreen(){
+  ctx.fillStyle = "#ffffff";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.font = "8px Arial";
+  ctx.fillStyle = "#000000";
+  ctx.fillText("Click to Start", window.width/2, window.height/2);
 }
